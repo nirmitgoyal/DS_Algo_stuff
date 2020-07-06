26b815e10c5ad67a2e64bc09a8f8dbd7446d5f5e
Nirmit Goyal <nirgoyal@amazon.com>
2020-07-07 00:01:49 +0530

    next commit

---
+++ src/LongestIncreasingSubsequence.java
@@ -0,0 +1,17 @@
+public class LongestIncreasingSubsequence {
+    int lengthOfLongestIncreasingSubsequence(int[] a) {
+        int[] cache = new int[n];
+
+        //fill cache with 1
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (a[i] > a[j]) {
+                    cache[i] = Math.max(cache[i] + 1, cache[i]);
+                }
+            }
+        }
+
+        //return the max value in cache
+    }
+}
