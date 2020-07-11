1f57b3eead8ee7170d050720c29b53d46c87434f
Nirmit Goyal <nirgoyal@amazon.com>
2020-07-11 15:08:25 +0530

    next

---
+++ src/SumOfSquares.java
@@ -0,0 +1,18 @@
+public class SumOfSquares {
+    int minElementsRequires(int[] list, int n) {//n > 0 //return value of Integer.MAX_VALUE means not possible
+        int[] minElementsRequired = new int[n + 1];
+        //fill with Integer.MAX_VALUE
+        minElementsRequired[0] = 0;
+
+        for (int i = 0; i < minElementsRequired.length; i++) {
+            for (int element : list) {
+                int indexToFill = i + element;
+                if (isSafe(indexToFill)) {
+                    int newCount = minElementsRequired[i] + 1;
+                    minElementsRequired[indexToFill] = Math.min(newCount, minElementsRequired[indexToFill]);
+                }
+            }
+        }
+        return minElementsRequired[n];
+    }
+}
