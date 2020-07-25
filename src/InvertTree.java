95f92a460316e7d1d289c5526d31467fda7e7928
Nirmit Goyal <nirgoyal@amazon.com>
2020-07-25 19:45:08 +0530

    next

---
+++ src/InvertTree.java
@@ -0,0 +1,16 @@
+import models.Node;
+
+public class InvertTree {
+
+    Node invert(Node node){
+        if (node==null)
+            return null;
+
+        Node left=invert(node.L);
+        Node right=invert(node.R);
+        node.L=right;
+        node.R=left;
+
+        return node;
+    }
+}
