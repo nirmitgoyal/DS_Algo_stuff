5d8e1dd406ad6037591dccb6ee28675045310453
Nirmit Goyal <nirgoyal@amazon.com>
2020-08-02 22:33:30 +0530

    next

---
+++ src/NQueens.java
@@ -0,0 +1,65 @@
+import models.Cell;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.fill;
+
+public class NQueens {
+
+    static List<Cell> placeNQueens(int n) {//this code does not works though
+        List<Cell> result = new ArrayList<>();
+
+        Boolean[] allowedRows = new Boolean[n];
+        fill(allowedRows, true);
+        Boolean[] allowedCols = new Boolean[n];
+        fill(allowedCols, true);
+        Boolean[] allowedRightDiagonals = new Boolean[2 * n - 1];
+        fill(allowedRightDiagonals, true);
+        Boolean[] allowedLeftDiagonals = new Boolean[2 * n - 1];
+        fill(allowedLeftDiagonals, true);
+
+        return placeNQueensHelper(n, allowedCols, allowedRows, allowedRightDiagonals, allowedLeftDiagonals, result);//send allowedCols for rows(to track rows)
+    }
+
+    private static List<Cell> placeNQueensHelper(int n, Boolean[] allowedRows, Boolean[] allowedCols, Boolean[] allowedRightDiagonals, Boolean[] allowedLeftDiagonals, List<Cell> result) {
+        if (result.size() == n)
+            return result;
+
+        int currRow = result.size();
+
+        for (int currCol = 0; currCol < n; currCol++) {
+            if (allowedRows[currCol] && allowedCols[currCol] && (!Utils.isSafe(currRow + currCol, n) || allowedRightDiagonals[currRow + currCol]) && (!Utils.isSafe(currRow - currCol, n) || allowedLeftDiagonals[currRow - currCol])) {
+                allowedRows[currRow] = false;
+                allowedCols[currCol] = false;
+                if (Utils.isSafe(currRow + currCol, n))
+                    allowedRightDiagonals[currRow + currCol] = false;
+                if (Utils.isSafe(currRow - currCol, n))
+                    allowedLeftDiagonals[currRow - currCol] = false;
+
+                result.add(new Cell(currRow, currCol));
+                placeNQueensHelper(n, allowedRows, allowedCols, allowedRightDiagonals, allowedLeftDiagonals, result);
+
+                if (result.size() == n)
+                    return result;
+
+                //backtrack
+                allowedRows[currRow] = true;
+                allowedCols[currCol] = true;
+                if (Utils.isSafe(currRow + currCol, n))
+                    allowedRightDiagonals[currRow + currCol] = true;
+                if (Utils.isSafe(currRow - currCol, n))
+                    allowedLeftDiagonals[currRow - currCol] = true;
+                result.remove(result.size() - 1);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(placeNQueens(5).toArray()));
+    }
+
+}
