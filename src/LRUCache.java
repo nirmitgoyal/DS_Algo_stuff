c5a3d7696ddd16df0aea6b9c005418aa19bb77ce
Nirmit Goyal <nirgoyal@amazon.com>
2021-01-12 11:44:41 +0530

    next

---
+++ src/LRUCache.java
@@ -0,0 +1,82 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRUCache {
+    int capacity;
+    Map<Integer, Node> map;
+    Node head, tail; //node just before tail will be LRU
+
+    LRUCache() {
+        capacity = 5;
+        map = new HashMap<>();
+
+        //2 dummy nodes
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        } else {
+            Node node = map.get(key);
+
+            update(node);
+
+            return node.value;
+        }
+    }
+
+    void put(int key, int value) {
+        if (!map.containsKey(key)) {
+            if (map.size() > capacity) {
+                Node LRU = tail.prev;
+                remove(LRU);
+                map.remove(LRU.key);
+            }
+            Node node = new Node(key, value);
+            map.put(key, node);
+            add(node);
+        } else {
+            Node node = map.get(key);
+
+            node.value = value;
+            update(node);
+        }
+    }
+
+    //////////////////////////////////////////////////////////////
+    //make these methods first:
+    private void add(Node node) { //adds just after head
+        Node next = head.next;
+
+        head.next = node;
+        node.prev = head;
+        node.next = next;
+        next.prev = node;
+    }
+
+    private void remove(Node node) { //removes the given node from anywhere
+        //these will never throw NPE coz we have 2 dummy nodes initially
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    //////////////////////////////////////////////////////////////
+    private void update(Node node) {
+        remove(node);
+        add(node);
+    }
+
+    private class Node {
+        int key, value;
+        Node prev, next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
