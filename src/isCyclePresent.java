2e07eb4a9ea4c5ffab0e23705ebefa2c770291bc
Nirmit Goyal <nirmitgoyal.goyal@gmail.com>
2021-01-16 00:23:59 +0530

    Update isCyclePresent.java

--- src/isCyclePresent.java
+++ src/isCyclePresent.java
@@ -8,7 +8,6 @@ public class isCyclePresent {
     static Map<Integer, Boolean> visited = new HashMap<>();
     static Map<Integer, Boolean> cache = new HashMap<>();
 
-    //O(n)
     boolean isCyclePresent(int[] nodes) {
         for (int i = 0; i < n; i++)
             if (DFSWithCache(i))
@@ -17,22 +16,21 @@ public class isCyclePresent {
         return false;
     }
 
-    boolean DFSWithoutCache(int node) {//become n^2 with this old approach
+    boolean DFS(int node) {//become overall O(n^2) with this old approach
         if (visited.containsKey(node) && visited.get(node) == true)
             return true;
 
         visited.put(node, true);
 
-        for (int currNode : getNeighbours(node)) {
-            if (DFSWithoutCache(currNode)==true)
+        for (int currNode : getNeighbours(node)) 
+            if (DFS(currNode) == true)
                 return true;
-        }
+        
         visited.put(node, false);
-
         return false;
     }
 
-    boolean DFSWithCache(int node) {
+    boolean DFSWithCache(int node) {//overall O(n) with this approach
         if (cache.containsKey(node))//for optimization
             return cache.get(node);
 
@@ -42,14 +40,13 @@ public class isCyclePresent {
         visited.put(node, true);
 
         boolean hasCycle = false;
-        for (int currNode : getNeighbours(node)) {
+        for (int currNode : getNeighbours(node)) 
             if (DFSWithCache(currNode)) {
                 hasCycle = true;
                 break;
             }
-        }
+        
         visited.put(node, false);
-
         cache.put(node, hasCycle);
         return hasCycle;
     }
