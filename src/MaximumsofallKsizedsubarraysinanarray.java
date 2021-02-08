6e78ad9a5e70a611ec719ad36d1fcd43aca020f8
Nirmit Goyal <nirgoyal@amazon.com>
2021-02-08 21:36:26 +0530

    n

--- src/MaximumsofallKsizedsubarraysinanarray.java
+++ src/MaximumsofallKsizedsubarraysinanarray.java
@@ -12,12 +12,12 @@ public class MaximumsofallKsizedsubarraysinanarray {
         for (int i = 0; i < n; i++) {
             //remove from front, if out of window
             if (!dq.isEmpty()
-             && dq.peekFirst() == (i - k))
+                    && dq.peekFirst() < i - k + 1)
                 dq.pollFirst();
 
-            //remove from back, if newcomer is >= last element
-            if (!dq.isEmpty()
-             && a[i] >= dq.peekLast())
+            //remove from back, if newcomer is >= every last element
+            while (!dq.isEmpty()
+                    && a[i] >= a[dq.peekLast()])
                 dq.pollLast();
 
             //Always insert me in DQ, bcoz you don't know the future elements
