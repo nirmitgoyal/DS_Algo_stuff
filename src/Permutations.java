47a2176b5cc25bc402ab721ea8c7935dc651e8d2
Nirmit Goyal <nirgoyal@amazon.com>
2021-02-10 13:38:47 +0530

    n

--- src/Permutations.java
+++ src/Permutations.java
@@ -1,27 +1,25 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static java.util.Collections.swap;
 
 //n!
 public class Permutations {
+    List<int[]> result = new ArrayList<>();
 
     void permutations(int[] a) {
-        ArrayList<int[]> result = new ArrayList<>();
-        permutationsHelper(0, a, result);
+        permutationsHelper(0, a);
     }
 
-    void permutationsHelper(int index, int[] a, ArrayList<int[]> result) {
+    void permutationsHelper(int index, int[] a) {
         int n = a.length;
-
-        if (index == n - 1) {
+        if (index == n - 1)
             result.add(a);
-            return;
-        }
 
         for (int i = index; i < n; i++) {
             swap(Arrays.asList(a), i, index);
-            permutationsHelper((i + 1), a, result); //i+1 coz we are fixing i now
+            permutationsHelper((i + 1), a); //i+1 coz we are fixing i now
             swap(Arrays.asList(a), i, index); //backtrack
         }
     }
