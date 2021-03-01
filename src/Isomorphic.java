f229b6e97d77058469a23cbc158dda37f213595e
Nirmit Goyal <nirgoyal@amazon.com>
2021-03-02 00:21:37 +0530

    next

--- src/Isomorphic.java
+++ src/Isomorphic.java
@@ -4,8 +4,7 @@ import java.util.Map;
 public class Isomorphic {
 
     boolean isIsomorphic(String s1, String s2) {
-        int n = s1.length();
-        if (n != s2.length())
+        if (s1.length() != s2.length())
             return false;
 
         return isIsomorphicHelper(s1, s2) && isIsomorphicHelper(s2, s1);
@@ -18,13 +17,10 @@ public class Isomorphic {
             char c1 = s1.charAt(i);
             char c2 = s2.charAt(i);
 
-            if (!map.containsKey(c1)) {
+            if (!map.containsKey(c1))
                 map.put(c1, c2);
-            } else {
-                char mapping = map.get(c1);
-                if (mapping != c2)
-                    return false;
-            }
+            else if (c2 != map.get(c1))
+                return false;
         }
 
         return true;
