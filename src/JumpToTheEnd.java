469e791c46257fe682e56fc7f2612bb290736dc2
Nirmit Goyal <nirgoyal@amazon.com>
2021-03-01 18:07:23 +0530

    next

--- src/JumpToTheEnd.java
+++ src/JumpToTheEnd.java
@@ -21,14 +21,38 @@ public class JumpToTheEnd {
         return minJumps[n - 1];
     }
 
-    int JumpToTheEnd_O_n(int[] a) {//3 2 5 1 1 9 3 4 // O(n)
+    public int JumpToTheEnd_O_n_Leetcode(int[] a) {
+        int n = a.length;
+        if (n <= 1)
+            return 0;
+
+        // max position one could reach starting from index <= i
+        int maxPos = a[0];
+        // max number of steps one could do inside this jump
+        int maxSteps = a[0];
+
+        int jumps = 0;
+        for (int i = 1; i < n; ++i) {
+            // if to reach this point one needs one more jump
+            if (maxSteps < i) {
+                jumps++;
+                maxSteps = maxPos;
+            }
+
+            maxPos = Math.max(maxPos, a[i] + i);
+        }
+
+        return jumps;
+    }
+
+    int JumpToTheEnd_O_n_Youtube(int[] a) {//3 2 5 1 1 9 3 4 // O(n)
         int n = a.length;
 
         if (n <= 1)
             return 0;
 
         int ladder = a[0], stairs = a[0];
-        int jumps = 1;
+        int jumps = 0;
 
         for (int i = 1; i < n; i++) {
             // consume a stair
