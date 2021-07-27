f0b590144725b1979a429c3dd80b53feada2c034
Nirmit <nirmit_goyal@intuit.com>
2021-07-27 22:26:42 +0530

    next

--- src/HIndex.java
+++ src/HIndex.java
@@ -3,7 +3,7 @@ public class HIndex {
     int hIndex(int[] a) {// 3 3 1 0 5
         int n = a.length;
         Integer[] map = new Integer[n + 1]; //1 based index//Eg: n = 5
-        int n2 = map.length;
+        int n2 = n+1;
 
         for (int e : a)
             if (e >= n) //
@@ -16,10 +16,10 @@ public class HIndex {
             map[i] += map[i + 1]; //number of publications with i+ references
         //5 4 3 3 1 1
 
-        for (int i = (n2 - 1); i >= 0; i--)
-            if (map[i] >= i)
-                return i; //not map[i]
-
-        return 0; //not 1
+        for (int i = 0; i < n2; i++) {
+            int _1BasedI = i + 1;
+            if (map[i] < _1BasedI)
+                return _1BasedI - 1;
+        }
     }
 }
--- src/RomanToInt.java
+++ src/RomanToInt.java
@@ -3,18 +3,17 @@ public class RomanToInt {
 //    init map: {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     int romanToInt(char[] s) {
-        int prev = map.get(s[n - 1]);
-        int sum = prev;
+        init();
+
+        int sum = map.get(s[n-1]);
 
         for (int i = n - 2; i >= 0; i--) {
             int curr = map.get(s[i]);
 
-            if (curr < prev)
-                sum -= curr;
-            else
+            if (curr >= map.get(s[i + 1]))
                 sum += curr;
-
-            prev = curr;
+            else
+                sum -= curr;
         }
 
         return sum;
