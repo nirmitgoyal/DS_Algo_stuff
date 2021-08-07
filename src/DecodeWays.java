6569be6667434a2467882b2c1c33ee80805cc3f7
Nirmit <nirmit_goyal@intuit.com>
2021-08-07 20:36:50 +0530

    next

--- src/DecodeWays.java
+++ src/DecodeWays.java
@@ -7,23 +7,25 @@ public class DecodeWays {
 
         int count = numDecodings(s.substring(1));
         if (s.length() >= 2
-                && ((s[0] == '1' || s[0] == '2') && s[1] <= '6')
-        )
+                && ((s[0] == '1' || s[0] == '2') && s[1] <= '6'))
             count += numDecodings(s.substring(2));
 
         return count;
     }
 
+    int n;
+    String s;
+
+    //then covert it to index based
     public int numDecodings(int index) {
         if (index == n)
             return 1;
 
 
-        count = numDecodings(index + 1);
+        int count = numDecodings(index + 1);
         if (index < n - 1
-                && ((s[index] == '1' || s[index] == '2') && s[index + 1] <= '6')
-        )
-            count += numDecodings(s.substring(index + 2));
+                && ((s[index] == '1' || s[index] == '2') && s[index + 1] <= '6'))
+            count += numDecodings(index + 2);
 
         return count;
     }
