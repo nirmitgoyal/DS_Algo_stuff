28a9eeb8d1cd3c2cc1961f6441d6e9fd150c20c8
Nirmit <nirmit_goyal@intuit.com>
2021-12-28 16:13:08 +0530

    next

--- src/DecodeWays.java
+++ src/DecodeWays.java
@@ -1,33 +1,59 @@
 public class DecodeWays {
 
-    public int numDecodings(String s) {
-        if (s.length() == 0)
-            return 1;
-
-
-        int count = numDecodings(s.substring(1));
-        if (s.length() >= 2
-                && ((s[0] == '1' || s[0] == '2') && s[1] <= '6'))
-            count += numDecodings(s.substring(2));
-
-        return count;
+  public int numDecodings(String s) {
+    //case 1
+    if (s.length() == 0) {//an empty string can only be decoded as an empty string -> 1 way
+      return 1;
     }
 
-    int n;
-    String s;
-
-    //then covert it to index based
-    public int numDecodings(int index) {
-        if (index == n)
-            return 1;
-
-
-        int count = numDecodings(index + 1);
-        if (index < n - 1
-                && ((s[index] == '1' || s[index] == '2') && s[index + 1] <= '6'))
-            count += numDecodings(index + 2);
-
-        return count;
+    //case 2
+    if (s.charAt(0) == '0') {
+      return 0;
     }
 
+    //case 3
+    if (s.length() == 1) {
+      return 1;
+    }
+
+    int count = numDecodings(s.substring(1));
+
+    //case 4
+    if (s.length() >= 2
+        && ((s.charAt(0) == '1' || s.charAt(0) == '2') && s.charAt(1) <= '6')) {
+      count += numDecodings(s.substring(2));
+    }
+
+    return count;
+  }
+
+
+  public int numDecodings2(String s) {
+    return numDecodingsHelper(s, 0);
+  }
+
+  //then covert it to index based
+  public int numDecodingsHelper(String s, int index) {
+    int n = s.length();
+
+    if (index == n) {//an empty string can only be decoded as an empty string -> 1 way
+      return 1;
+    }
+
+    if (s.charAt(index) == '0') {
+      return 0;
+    }
+
+    if (index == n - 1) {
+      return 1;
+    }
+
+    int count = numDecodingsHelper(s, index + 1);
+    if (index < n - 1
+        && ((s.charAt(index) == '1' || s.charAt(index) == '2') && s.charAt(index + 1) <= '6')) {
+      count += numDecodingsHelper(s, index + 2);
+    }
+
+    return count;
+  }
 }
