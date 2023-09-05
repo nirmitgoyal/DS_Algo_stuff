0cbb63ec4f3fe2d180510e5e359f36f06cc5e390
Nirmit <nirmit_goyal@intuit.com>
2023-09-05 12:09:47 +0530

    next

--- src/CountUniqueValueSubtrees.java
+++ src/CountUniqueValueSubtrees.java
@@ -2,27 +2,31 @@ import lombok.AllArgsConstructor;
 import models.Node;
 
 public class CountUniqueValueSubtrees {
-    int countUniqueValueSubtrees(Node node) {
-        return countUniqueValueSubtreesHelper(node).count;
+
+  int countUniqueValueSubtrees(Node node) {
+    return countUniqueValueSubtreesHelper(node).count;
+  }
+
+  private Return countUniqueValueSubtreesHelper(Node node) {
+    if (node == null) {
+      return new Return(true, 0);
     }
 
-    private Return countUniqueValueSubtreesHelper(Node node) {
-        if (node == null)
-            return new Return(true, 0);
+    Return left = countUniqueValueSubtreesHelper(node.L);
+    Return right = countUniqueValueSubtreesHelper(node.R);
 
-        Return left = countUniqueValueSubtreesHelper(node.L);
-        Return right = countUniqueValueSubtreesHelper(node.R);
-
-        if (left.isUniqueValue && right.isUniqueValue
-                && (node.L == null || node.data == node.L.data) && (node.R == null || node.data == node.R.data))
-            return new Return(true, left.count + right.count + 1);
-
-        return new Return(false, left.count + right.count);
+    if (left.isUniqueValue && right.isUniqueValue
+        && (node.L.data == node.data || node.L == null) && (node.R.data == node.data || node.R == null)) {
+      return new Return(true, left.count + right.count + 1);
+    } else {
+      return new Return(false, left.count + right.count);
     }
+  }
 
-    @AllArgsConstructor
-    class Return {
-        boolean isUniqueValue;
-        int count;
-    }
+  @AllArgsConstructor
+  class Return {
+
+    boolean isUniqueValue;
+    int count;
+  }
 }
