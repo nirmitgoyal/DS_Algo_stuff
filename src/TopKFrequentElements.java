a450c2f5d5ae27603f2e4552c9002a41e1df9465
Nirmit <nirmitgoyal.goyal@gmail.com>
2023-09-14 15:30:37 +0530

    code

--- src/TopKFrequentElements.java
+++ src/TopKFrequentElements.java
@@ -3,6 +3,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.PriorityQueue;
 import lombok.AllArgsConstructor;
 
@@ -13,27 +14,26 @@ public class TopKMostFrequentElements {
     Map<Integer, Integer> map = new HashMap<>(); //data -> count
     PriorityQueue<Element> pq = new PriorityQueue<>(new CustomComparator());
 
-      for (int e : a) {
-          map.put(e, (map.getOrDefault(e, 0) + 1));
-      }
+    for (int e : a) {
+      map.put(e, (map.getOrDefault(e, 0) + 1));
+    }
 
-    map.forEach((data, count) -> {
+    for (Integer data: map.keySet()) {
+      Integer count = map.get(data);
       pq.add(new Element(data, count));
-    });
+    }
 
-      for (int i = 0; i < k; i++) //assuming atleast k elements are there
-      {
-          result.add(pq.poll().data);
-      }
+    for (int i = 0; i < k; i++) { //assuming atleast k elements are there
+      result.add(pq.poll().data);
+    }
 
     return result;
   }
 
   // CustomComparator compare template
   class CustomComparator implements Comparator<Element> {
-
     public int compare(Element e1, Element e2) {
-      return -e1.count.compareTo(e2.count);
+      return e2.count.compareTo(e1.count);
     }
   }
 
