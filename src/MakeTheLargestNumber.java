51982158290a63cc5a263eb98975da3c35129ea6
Nirmit Goyal <nirmitgoyal.goyal@gmail.com>
2023-09-16 19:27:33 +0530

    Update MakeTheLargestNumber.java

--- src/MakeTheLargestNumber.java
+++ src/MakeTheLargestNumber.java
@@ -9,28 +9,28 @@ public class MakeTheLargestNumber {
       String order1 = a + b;
       String order2 = b + a;
 
-      return -order1.compareTo(order2);
+      return order2.compareTo(order1);
     }
   }
 
   public String largestNumber(int[] nums) {
     // Get input integers as strings.
-    String[] asStrs = new String[nums.length];
+    String[] numsStrings = new String[nums.length];
     for (int i = 0; i < nums.length; i++) {
-      asStrs[i] = String.valueOf(nums[i]);
+      numsStrings[i] = String.valueOf(nums[i]);
     }
 
     // Sort strings according to custom comparator.
-    Arrays.sort(asStrs, new CustomComparator());
+    Arrays.sort(numsStrings, new CustomComparator());
 
     // If, after being sorted, the largest number is `0`, the entire number is zero.
-    if (asStrs[0].equals("0")) {
+    if (numsStrings[0].equals("0")) {
       return "0";
     }
 
     // Build largest number from sorted array.
     String ans = "";
-    for (String s : asStrs) {
+    for (String s : numsStrings) {
       ans += s;
     }
 
