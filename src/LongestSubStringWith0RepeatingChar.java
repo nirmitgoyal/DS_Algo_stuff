4ef789185f37aed6f409f770511fad863b56bcd0
Nirmit Goyal <nirmitgoyal.goyal@gmail.com>
2026-09-07 11:17:27 +0530

    Update LongestSubStringWith0RepeatingChar.java

--- src/LongestSubStringWith0RepeatingChar.java
+++ src/LongestSubStringWith0RepeatingChar.java
@@ -9,19 +9,15 @@ public class LongestSubStringWith0RepeatingChar {
     int n = s.length;
   
     Map<Character, Integer> map = new HashMap<>();
-    int
-        nextStartIndex = 0,
-        maxLength = 1;
+    int nextStartIndex = 0, maxLength = 1;
 
     for (int i = 0; i < n; i++) {
       char c = s[i];
 
-      if (map.containsKey(c)) {
+      if (map.containsKey(c)) 
         nextStartIndex = map.get(c) + 1;
-      }
 
       maxLength = max(maxLength, i - nextStartIndex + 1);
-
       map.put(c, i);
     }
 
