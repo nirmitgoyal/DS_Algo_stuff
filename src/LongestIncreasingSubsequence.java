9377e72316421d8780d4bbc66d3ccb330fc4ee34
Nirmit Goyal <nirmitgoyal.goyal@gmail.com>
2026-09-09 13:45:20 +0530

    Update LongestIncreasingSubsequence.java

--- src/LongestIncreasingSubsequence.java
+++ src/LongestIncreasingSubsequence.java
@@ -10,26 +10,21 @@ public class LongestIncreasingSubsequence {
     //no base needed
 
     int ans = 1;
-    for (int i = 0; i < index; i++) {
-      if (a[index] > a[i]) {
+    for (int i = 0; i < index; i++) 
+      if (a[index] > a[i]) 
         ans = max(ans, longestIncreasingSubsequenceLength(a, i) + 1);
-      }
-    }
 
     return ans;
   }
 
   int longestIncreasingSubsequenceLength2(int[] a, int index) { //O(n)
-    if (cache.containsKey(index)) {
+    if (cache.containsKey(index)) 
       return cache.get(index);
-    }
 
     int ans = 1;
-    for (int i = 0; i < index; i++) {
-      if (a[index] > a[i]) {
+    for (int i = 0; i < index; i++) 
+      if (a[index] > a[i]) 
         ans = max(ans, longestIncreasingSubsequenceLength(a, i) + 1);
-      }
-    }
 
     cache.put(index, ans);
     return ans;
